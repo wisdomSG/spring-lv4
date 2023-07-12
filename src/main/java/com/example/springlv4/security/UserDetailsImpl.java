017f4376ad4759142d7af0724777656e7b576fe5
seulgi <asd0927122@gmail.com>
2023-07-12 17:00:06 +0900

    WebSecurity 사용

---
+++ src/main/java/com/example/springlv4/security/UserDetailsImpl.java
@@ -0,0 +1,53 @@
+package com.example.springlv4.security;
+
+import com.example.springlv4.entity.User;
+import org.antlr.v4.runtime.misc.Array2DHashSet;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/example/springlv4/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.springlv4.security;
+
+import com.example.springlv4.entity.User;
+import com.example.springlv4.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(()->
+                new UsernameNotFoundException("username not found: " + username));
+
+        return new UserDetailsImpl(user);
+    }
+
+
+
+}
