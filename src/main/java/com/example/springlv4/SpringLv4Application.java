ed75ec1c36fb33b83b0147e40dfd824e466c18c0
seulgi <asd0927122@gmail.com>
2023-07-12 18:48:59 +0900

    @EnableJpaAuditing, 조회 기능 허가

--- src/main/java/com/example/springlv4/SpringLv4Application.java
+++ src/main/java/com/example/springlv4/SpringLv4Application.java
@@ -3,8 +3,10 @@ package com.example.springlv4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing      //Timestamped 시간을 체크해주는 기능을 사용하기위한 애너테이션
 public class SpringLv4Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springlv4/config/WebSecurityConfig.java
+++ src/main/java/com/example/springlv4/config/WebSecurityConfig.java
@@ -53,7 +53,9 @@ public class WebSecurityConfig {
                         authorizeHttpRequests
                                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                                 .requestMatchers("/api/user/**").permitAll() // '/api'로 시작하는 요청 모두 접근 허가
+                                .requestMatchers(HttpMethod.GET, "/api/post/**").permitAll() // 조회 메서드 허용
                                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+
         );
 
         // 필터 관리
